b75c7647b9fdf01c10c1eaef9547da80d9ec0880
Etienne Soucy <esoucy@myself.com>
2026-02-22 10:58:11 -0500

    refactor(test): parametrize tokenize tests

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,23 +1,24 @@
+import pytest
+
 from critter_world.token import Tokenizer, Type
 
 
-def test_tokenize_token_plus():
-    """Tokenize '+'.
+@pytest.mark.parametrize(
+    ('string', 'expected'),
+    [
+        ('+', Type.PLUS),
+        ('-', Type.MINUS),
+    ],
+)
+def test_tokenize_single_token(string: str, expected: Type):
+    """Tokenize single tokens.
 
-    ["+", Type.PLUS]
-    ["-", Type.MINUS]
     ["*", Type.MUL]
     ["/", Type.DIV]
     ["+", Type.PLUS]
     ["-->", Type.THEN]
+
     """
     tokenizer = Tokenizer()
-    token = tokenizer.tokenize('+')[0]
-    assert token.type == Type.PLUS
-
-
-def test_tokenize_token_minus():
-    """Tokenize '-'."""
-    tokenizer = Tokenizer()
-    token = tokenizer.tokenize('-')[0]
-    assert token.type == Type.MINUS
+    token = tokenizer.tokenize(string)[0]
+    assert token.type == expected
